a6136c269c029f784fae1a0a35e154db9b95b164
KronosDP <darrel.danadyaksa19@gmail.com>
2024-04-05 10:40:49 +0700

    Create Notification service struct skeleton

--- src/controller/mod.rs
+++ src/controller/mod.rs
@@ -1,10 +1,20 @@
+pub mod notification;
 pub mod product;
-
 use rocket::fairing::AdHoc;
 
 pub fn route_stage() -> AdHoc {
-    return AdHoc::on_ignite("Initializing controller routes...", |rocket| async {
+    return AdHoc::on_ignite("Initializing controller routes...", |rocket| async move {
         rocket
-            .mount("/product", routes![product::create, product::list, product::read, product::delete])
+            .mount(
+                "/product",
+                routes![
+                    product::create,
+                    product::list,
+                    product::list,
+                    product::read,
+                    product::delete
+                ],
+            )
+            .mount("/notification", routes![]);
     });
 }
